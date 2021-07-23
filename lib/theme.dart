ede03a56bc9593b3ed3f2b48f09599ba7a027058
Arief Syahroni <riefsyah31@gmail.com>
2021-07-23 09:06:29 +0700

    setting up a project and add some assets

---
+++ lib/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final double defaultMargin = 30.0;
+
+Color purpleColor = Color(0XFF312651);
+Color whiteColor = Color(0XFFFFFFFF);
+Color blackColor = Color(0XFF000000);
+Color lightGreyColor = Color(0XFFF3F4F8);
+Color lightPurpleColor = Color(0XFF858496);
+Color orangeColor = Color(0XFFFF7755);
+
+TextStyle whiteTitleTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+
+TextStyle blackTitleTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+
+TextStyle subTitleTextStyle = GoogleFonts.poppins();
